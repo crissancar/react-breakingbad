ce030979ab874b32d704f76e41859319fad187f3
Cristian <cristiansanchezcarr@gmail.com>
2020-05-14 16:30:50 +0200

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import styled from '@emotion/styled';
+import Quote from './components/Quote.js';
+
+//Componente contenedor
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top: 5rem;
+  flex-direction: column;
+`;
+
+//Componente botón de obtener Cita
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  backgournd-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+  transition: background-size .8s ease;
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
+`;
 
 function App() {
+
+  //State de las frases
+  const [quote, saveQuote] = useState({});
+
+  //Llamar a la api
+  /*
+    ##########
+    Forma tradicional
+    ##########
+  */
+  const queryAPI = () => {
+    //Llamada
+    const api = fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    //Promesa
+    const promise = api.then(response => response.json());
+    promise.then(result => console.log(result));
+  }
+
+  /*
+    ##########
+    Forma moderna
+    ##########
+    await detiene la ejecución del código hasta que se cumpla la promesa
+  */
+  const queryAPIasync = async () => {
+    //Llamada
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    //Promesa
+    const promise = await api.json();
+    saveQuote(promise[0]);
+  }
+
+  //State para cargar una frase automáticamente al regargar la página
+  useEffect(() => {
+    queryAPIasync();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Quote
+        quote={quote}
+      />
+      <Button
+        onClick={queryAPIasync}
+      >
+        Cita
+      </Button>
+    </Container>
   );
 }
 
